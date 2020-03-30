2cd9b32faf001983910c2cd802d5c3946e58a1df
mrtrashmind <znrfire@gmail.com>
2020-03-30 16:47:01 +0400

    add var

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let start = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelresultValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+    inputExpenses = document.getElementsByClassName('expenses-item'),
+    buttonExpenses = document.getElementsByTagName('button')[0],
+    buttonOptionalExpenses = document.getElementsByTagName('button')[1],
+    buttonCountBudget = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('.checksavings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
+
+
